d580ec8d34b17da88709c206451d8b4555313f71
Mariana <mariana@fastmail.net>
2021-12-23 09:30:27 +0100

    setup UI

---
+++ HotCoffee/Controllers/OrdersTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  OrdersTableViewController.swift
+//  HotCoffee
+//
+//  Created by Mariana Steblii on 23/12/2021.
+//
+// MARK: - IBOutlets
+// MARK: - Private properties
+// MARK: - Public properties
+// MARK: - View functions
+// MARK: - Private functions
+// MARK: - Public functions
+// MARK: - @objc private functions
+// MARK: - IBActions
+
+import UIKit
+
+class OrdersTableViewController: UITableViewController {
+    
+}
